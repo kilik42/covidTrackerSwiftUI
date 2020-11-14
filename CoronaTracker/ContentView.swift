959ccefd9d16848ee5f1c39e7eb7f47ef6e3bde3
marvin evins <marvinevins@yahoo.com>
2020-11-14 12:13:25 -0600

    Initial Commit

---
+++ CoronaTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoronaTracker
+//
+//  Created by marvin evins on 11/14/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoronaTracker/CoronaTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoronaTrackerApp.swift
+//  CoronaTracker
+//
+//  Created by marvin evins on 11/14/20.
+//
+
+import SwiftUI
+
+@main
+struct CoronaTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
